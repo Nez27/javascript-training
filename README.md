252315cbc2e77ac028bf749dc1cb45aa31c1641b
Loi Phan <phanhuuloi15@gmail.com>
2023-08-07 16:38:40 +0700

    Init value and variable samples

--- README.md
+++
@@ -1,2 +0,0 @@
-# javascript-training
-loi-phan-internship
---
+++ Sample/value_and_variables/index.js
@@ -0,0 +1,20 @@
+console.log("Nezumi");
+console.log(18);
+
+let lastName = "Phan";
+
+console.log(lastName);
+
+let nezumi = "Nez";
+let $function = 236;
+
+let person = "Mimimi";
+let PI = 3.1415;
+
+let myAge = 112;
+let myCurrentJob = "Grab bike";
+
+let firstJob = "student";
+let secondJob = "flight attendant";
+
+console.log(firstJob);
