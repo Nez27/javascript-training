ccafdecc10c211ff73f435d07d7c08933ff124e6
Loi Phan <phanhuuloi15@gmail.com>
2023-08-07 16:45:01 +0700

    Init data type sample

---
+++ Sample/data_types/index.js
@@ -0,0 +1,33 @@
+// Data Types
+let isBoolean = true;
+console.log(isBoolean);
+
+// console.log(typeof false);
+console.log(typeof isBoolean);
+// console.log(typeof 18);
+// console.log(typeof 'String text');
+
+isBoolean = "String text";
+console.log(typeof isBoolean);
+
+let number = 18;
+console.log(number);
+console.log(typeof number);
+
+number = 27;
+console.log(typeof number);
+
+console.log(typeof null);
+
+// let, const and var
+let day = 30;
+day = 31;
+
+const birthYear = 2001;
+// birthYear = 1990;
+
+var myJob = "Student";
+myJob = "Grab bike";
+
+lastName = "Nezumi";
+console.log(lastName);
